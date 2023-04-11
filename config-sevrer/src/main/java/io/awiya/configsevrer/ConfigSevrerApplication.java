8c9976adb3908a0e3a2a92c104a9f01210e9540a
AYOUB <a.idihia@gmail.com>
2023-04-11 18:58:12 +0200

    first version

---
+++ config-sevrer/src/main/java/io/awiya/configsevrer/ConfigSevrerApplication.java
@@ -0,0 +1,13 @@
+package io.awiya.configsevrer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigSevrerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigSevrerApplication.class, args);
+	}
+
+}
---
+++ config-sevrer/src/test/java/io/awiya/configsevrer/ConfigSevrerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.awiya.configsevrer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigSevrerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
