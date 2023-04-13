f1a495921abcf285c37072e503745f45ade08736
AYOUB <a.idihia@gmail.com>
2023-04-13 13:37:46 +0200

    Creation de la table et Autoriser Angular à accéder au Back

--- inventory--service/src/main/java/io/awiya/inventory/web/ProductController.java
+++ inventory--service/src/main/java/io/awiya/inventory/web/ProductController.java
@@ -4,16 +4,14 @@ import io.awiya.inventory.entities.Product;
 import io.awiya.inventory.exception.ProductNotFoundException;
 import io.awiya.inventory.repo.ProductRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200")
 public class ProductController {
 
     private final ProductRepository productRepository;
